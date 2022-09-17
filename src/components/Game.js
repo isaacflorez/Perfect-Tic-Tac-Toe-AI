ef4f0439b9a7f2c87520ff130db2e57b68db6ae8
Isaac Florez <69365833+isaacflorez@users.noreply.github.com>
2022-09-16 19:47:31 -0700

    add square function to print location when clicked

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,7 @@ import Board from './Board'
 import Message from "./Message"
 import { useState } from "react"
 const Game = () => {
+  // boardData is passed to Board in order to render squares
   const [boardData, setBoardData] = useState(
     [
       ['', '', ''],
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,12 @@
 import './Square.css'
 const Square = (props) => {
   return (
-    <button onClick={printLocation(props.location)} className="square" id={props.id}>{props.value}</button>
+    <button onClick={() => printLocation(props.location)} className="square" id={props.id}>{props.value}</button>
   )
 }
 
 export default Square
 
 let printLocation = (location) => {
-  // console.log(`Clicked at (${location[0]}, ${location[1]})`)
-  console.log(location)
+  console.log(`Clicked at (${location[0]}, ${location[1]})`)
 }
