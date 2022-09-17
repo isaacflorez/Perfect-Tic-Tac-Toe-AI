4317d343ab485cd73a95073700fa77debf25c262
Isaac Florez <69365833+isaacflorez@users.noreply.github.com>
2022-09-17 00:28:21 -0700

    added useEffect with isWinning function to check board when board state is updated

--- src/components/Context/BoardContext.js
+++ src/components/Context/BoardContext.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const BoardContext = React.createContext()
 export function ListProvider({ children }){
-    const [currentPlayer, setCurrentPlayer] = useState('x')
+    const [message, setMessage] = useState('may luck be in your favor')
+    const [currentPlayer, setCurrentPlayer] = useState('O')
+    const [gameOver, setGameOver] = useState(false)
     const [boardData, setBoardData] = useState(
         [
             ['', '', ''],
@@ -10,23 +12,72 @@ export function ListProvider({ children }){
             ['', '', '']
         ]
     )
+    const WINNING_COMBOS = [
+        [ [0,0], [0,1], [0,2] ],    // top row
+        [ [1,0], [1,1], [1,2] ],    // middle row
+        [ [2,0], [2,1], [2,2] ],    // bottom row
+        [ [0,0], [1,0], [2,0] ],    // left col
+        [ [0,1], [1,1], [2,1] ],    // middle col
+        [ [0,2], [1,2], [2,2] ],    // right col
+        [ [0,0], [1,1], [2,2] ],    // tl diagnol to br
+        [ [0,2], [1,1], [2,0] ]     // tr diagnol to bl
+    ]
 
-    let checkMove = (location) => {                                   
-        let [x, y] = [location[0], location[1]]     // get coordinates
-        if(boardData[x][y] === ''){                 // check if spot on the board is empty
-            setBoardData(addMoveToBoard(x,y))       // update board with new move
+    useEffect(() => {
+        // if winner then
+            // change message
+            // freeze board
+        // else
+            // change player
+        if(isWinner()){
+            setMessage(`${currentPlayer} is the winner`)
+            setGameOver(true)
+            setCurrentPlayer('X')
         } else {
-            console.log('clicked filled space')
+            currentPlayer === 'X'
+            ? setCurrentPlayer('O') 
+            : setCurrentPlayer('X')
         }
+    }, [boardData])
+
+
+    // see if move is valid or not
+    let checkMove = (location) => {
+        if (!gameOver){
+            let [x, y] = [location[0], location[1]]     // get coordinates
+            if(boardData[x][y] === ''){                 // check if spot on the board is empty
+                addMoveToBoard(x,y)                     // update board with new move
+            } else {
+                console.log('clicked filled space')
+            }
+        } else {
+            alert('reset board to play again')
+        }                         
+        
     }
 
+
+    // take given coordinates and add move for curr player
     let addMoveToBoard = (x,y) => {
-        let temp = boardData            // copy current board
+        let temp = [...boardData]       // copy current board
         temp[x][y] = currentPlayer      // mark square
-        currentPlayer === 'x'           // change player
-            ? setCurrentPlayer('o') 
-            : setCurrentPlayer('x')
-        return temp                     // return copy
+        setBoardData(temp)
+    }
+
+    // check if board contains any of the winning combos
+    // FIND A BETTER WAY TO DO THIS (:
+    let isWinner = () => {
+        let n = WINNING_COMBOS.length
+        for(let i = 0; i < n; i++){
+            let combo = WINNING_COMBOS[i]
+            let a = boardData[combo[0][0]][combo[0][1]]
+            let b = boardData[combo[1][0]][combo[1][1]]
+            let c = boardData[combo[2][0]][combo[2][1]]
+            if(a !== '' && a === b && b === c){
+                return true
+            }
+        }
+        return false
     }
 
     let cleanBoard = () => {        // resets board with empty values
@@ -37,11 +88,13 @@ export function ListProvider({ children }){
                 ['', '', '']
             ]
         )
+        setGameOver(false)
+        setMessage('may luck be in your favor')
     }
-    
+
     const context = {
         actions: {checkMove, cleanBoard},
-        state: {boardData, setBoardData}
+        state: {boardData, setBoardData, message}
     }
 
     return (
--- src/components/Message.js
+++ src/components/Message.js
@@ -3,10 +3,9 @@ import { useContext } from 'react'
 import './Message.css'
 const Message = () => {
     const {context} = useContext(BoardContext)
-    const message = "may the gods protect you"
     return (
         <section className='gameover'>
-            <p className="gameover_message">{message}</p>
+            <p className="gameover_message">{context.state.message}</p>
             <button onClick={() => context.actions.cleanBoard()} className="reset_board">reset board</button>
         </section>
     )
